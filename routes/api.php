1a79043ed1090b9a47e7fb9ce49d3f454b808d01
farhanrivaldy <farhanrivaldy92@gmail.com>
2023-08-28 04:07:12 +0700

    fist commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('addProduct', [ProductController::class, 'addProduct']);
+Route::get('list', [ProductController::class, 'list']);
+Route::delete('delete/{id}', [ProductController::class, 'delete']);
